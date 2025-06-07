25f7a2b5597095becb8163993651c2c6a1783bcb
mateus <mateus.jimenez@sptech.school>
2025-06-07 18:11:08 -0300

    atualização da model de inserção do quiz

--- Website/src/models/quizModel.js
+++ Website/src/models/quizModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function guardar(quiz, certas, erradas, usuario, tempo, porcentagem) {
     console.log("Executando model guardar:", quiz, certas, erradas, usuario, tempo, porcentagem);
     var instrucaoSql = `
-        INSERT INTO quiz (idQuiz, acertos, erros, fkusuario, tempo, porcentagem, dtRealizacao) 
+        INSERT INTO resultado (fkquiz, acertos, erros, fkusuario, tempo, porcentagem, dtRealizacao) 
         VALUES ('${quiz}', '${certas}', '${erradas}', '${usuario}', '${tempo}', '${porcentagem}', CURDATE());
     `;
     console.log("Executando SQL: \n" + instrucaoSql);
